87b08a744751833670a27a106731c4f53b0b3999
ilarramendi <ilarramendi1@gmail.com>
2022-06-24 00:09:07 -0300

    comment from python script

---
+++ BackupSettings.py
@@ -0,0 +1,13 @@
+from git import Repo
+
+PATH_OF_GIT_REPO = ".git/"  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push():
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(all=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+git_push()
